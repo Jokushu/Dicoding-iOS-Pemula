0e8b715f8cde08253b340afc12dd47f2c433824e
Jokushu <gnwnvincent@gmail.com>
2022-07-03 19:06:19 +0700

    Initial Commit

---
+++ Listaurant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Listaurant
+//
+//  Created by Vincent Gunawan on 03/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Listaurant/ListaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListaurantApp.swift
+//  Listaurant
+//
+//  Created by Vincent Gunawan on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ListaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
